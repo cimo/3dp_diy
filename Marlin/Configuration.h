dd23622d7245d30f7f70b533023a71cd072a9eb2
cimo <simone.dagostino@gmail.com>
2024-03-25 00:05:15 +0900

    update

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1331,7 +1331,7 @@
  */
 #if DISABLED(CLASSIC_JERK)
   // cimo +
-  #define JUNCTION_DEVIATION_MM 0.014 // (mm) Distance from real junction edge
+  #define JUNCTION_DEVIATION_MM 0.024 // (mm) Distance from real junction edge
   #define JD_HANDLE_SMALL_SEGMENTS    // Use curvature estimation instead of just the junction angle
                                       // for small segments (< 1mm) with large junction angles (> 135°).
 #endif
